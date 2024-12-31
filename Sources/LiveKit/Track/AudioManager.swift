4108f4c6a0e3bf731753504f2ac11e03a721d200
Andrii Deneha <andriid@cardosystems.com>
2024-12-31 16:51:33 +0200

    VoBLE: stop playout for track

--- Sources/LiveKit/Track/AudioManager.swift
+++ Sources/LiveKit/Track/AudioManager.swift
@@ -247,6 +247,8 @@ public class AudioManager: Loggable {
         }
         #if os(iOS) || os(visionOS) || os(tvOS)
         try await _asyncConfigure(newState: newState, oldState: oldState)
+        // STOP A2DP play
+        RTC.audioDeviceModule.stopPlayout()
         #endif
     }
 
