57b249aabc56e93bcd6040efd6296ef800e908dd
Andrii Deneha <andriid@cardosystems.com>
2025-05-23 18:41:19 +0300

    VoBLE: remove commented code

--- Sources/LiveKit/Track/AudioManager.swift
+++ Sources/LiveKit/Track/AudioManager.swift
@@ -247,8 +247,6 @@ public class AudioManager: Loggable {
         }
         #if os(iOS) || os(visionOS) || os(tvOS)
         try await _asyncConfigure(newState: newState, oldState: oldState)
-        // STOP A2DP play
-//        RTC.audioDeviceModule.stopPlayout()
         #endif
     }
 
